4ab417cc2efcd3e6b14dbfd5d073b950c5ae0adf
sandeep8558 <sandeep198558@gmail.com>
2026-08-12 08:18:19 +0530

    Set package name and product bundle identifier to com.infoleena.cloud.nas across Android, iOS, and macOS

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.cloudnas.app"
+    namespace = "com.infoleena.cloud.nas"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.cloudnas.app"
+        applicationId = "com.infoleena.cloud.nas"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
